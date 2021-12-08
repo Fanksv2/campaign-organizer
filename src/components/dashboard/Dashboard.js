b0690d7f6d80141751687e8718721125958b8363
Gabriel Arruda <gabriel.arruda@oktagon.com.br>
2021-12-07 22:38:49 -0300

    new: base dashboard screen

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,17 @@
 import React from "react";
+import BaseListLarge from "../base/BaseListLarge";
+import BaseMultipleContent from "../base/BaseMultipleContent";
+import LargeButton from "../base/LargeButton";
+import "../../styles/dashboard.css";
 
 const Dashboard = (props) => {
-    return <div>Dashboard</div>;
+    return (
+        <div className="dashboard">
+            <BaseMultipleContent title="Dashboard">
+                <div className="dashboard-content"></div>
+            </BaseMultipleContent>
+        </div>
+    );
 };
 
 export default Dashboard;
